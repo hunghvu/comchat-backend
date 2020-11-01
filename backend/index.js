a9fc904b53f2f98d3856142d1a62466ce5525c7b
Phong Hoang Le <phongfly@uw.edu>
2020-10-31 20:06:35 -0700

    Add auth endpoint.

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,15 @@ const express = require('express')
 //Create a new instance of express
 const app = express()
 
-//let middleware = require('./utilities/middleware')
+let middleware = require('./utilities/middleware')
 
 const bodyParser = require("body-parser");
 //This allows parsing of the body of POST requests, that are encoded in JSON
 app.use(bodyParser.json())
 
-//Obtain a Pool of DB connections. 
-const { Pool } = require('pg')
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    }
-})
+app.use('/auth', require('./routes/register.js')) 
 
-
-
-
+app.use('/auth', require('./routes/login.js')) 
 
 /*
  * This middleware function will respond to inproperly formed JSON in 
